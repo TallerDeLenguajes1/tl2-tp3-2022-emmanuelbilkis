668fb57a4d10d8c3b5444ca55b6b37001f7617c0
EmmanuelNBilkis <emmanuelbilkis@gmail.com>
2022-11-02 18:10:07 -0300

    cadetes ok

---
+++ ServiciosdeCadetes/ServiciosdeCadetes/Cadete.cs
@@ -0,0 +1,26 @@
+﻿namespace ServiciosdeCadetes
+{
+    class Cadete : Persona
+    {
+        List<Pedido> pedidos;
+
+        public Cadete(int idN, string nombreN, string direc, string tel, List<Pedido> pedidos) : base(idN, nombreN, direc, tel)
+        {
+            this.pedidos = pedidos;
+        }
+
+        public float jornalACobrar()
+        {
+            return 300 * this.pedidos.Count();
+        }
+
+        public void asignar(Cadete cadete)
+        {
+            foreach (var pedido in pedidos)
+            {
+                pedido.asignarCadete(cadete);
+            }
+        }
+
+    }
+}
---
+++ ServiciosdeCadetes/ServiciosdeCadetes/Program.cs
@@ -0,0 +1,43 @@
+﻿using ServiciosdeCadetes;
+
+namespace principal
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Cliente Nicolas = new Cliente(1, "Nicolas", "Saavedra Lamas 952", "4535345", "Casi Pte Peron");
+
+            Pedido pedido1 = new Pedido(1, "fsdfds", "en proceso", Nicolas, 2500);
+            Pedido pedido2 = new Pedido(2, "fsdfds", "en proceso", Nicolas, 1500);
+            Pedido pedido3 = new Pedido(3, "fsdfds", "en proceso", Nicolas, 800);
+            Pedido pedido4 = new Pedido(4, "fsdfds", "en proceso", Nicolas, 1200);
+
+
+
+            List<Pedido> listaPedidos = new List<Pedido>();
+            listaPedidos.Add(pedido1);
+            listaPedidos.Add(pedido2);
+            listaPedidos.Add(pedido3);
+            listaPedidos.Add(pedido4);
+
+            Cadete Lautaro = new Cadete(1, "Lautaro", "Saavedra Lamas 952", "4535345", listaPedidos);
+
+            pedido1.asignarCadete(Lautaro);
+            pedido2.asignarCadete(Lautaro);
+            pedido3.asignarCadete(Lautaro);
+            pedido4.asignarCadete(Lautaro);
+
+            foreach (var item in listaPedidos)
+            {
+                item.mostrarCadete();
+            }
+
+            Console.WriteLine($"El jornal a cobrar es: {Lautaro.jornalACobrar()}");
+
+        }
+
+
+    }
+}
+
